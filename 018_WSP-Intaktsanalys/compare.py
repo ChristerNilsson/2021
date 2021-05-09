d5fbeaf7666623e8b40dbd91d45e60afdc67c4bd
ChristerNilsson <janchrister.nilsson@gmail.com>
2021-05-09 02:14:52 +0200

    compare

--- 018_WSP-Intaktsanalys/compare.py
+++ 018_WSP-Intaktsanalys/compare.py
@@ -1,27 +1,30 @@
 import pandas as pd
 import time
 
-# Du kan begränsa jämförandet på några olika sätt:
+# Du kan styra jämförandet på flera olika sätt:
 #  0. Välj kataloger med DIR1 och DIR2
-#  1. I slutet av denna fil kan du påverka vila filer du vill jämföra. Byt mellan if 1 och if 0
-#  2. Byt range(rows) till t ex range(min(1000,rows))
-#  3. Påverka antal signifikanta siffror genom att ändra .7e till .6e eller något annat
-#  4. Vill du se bara första problemet, avkommentera "assert a==b"
+#  1. Sätt ROWS = 1000 om du vill ha maximalt 1000 rader jämförda.
+#  2. Vill du se bara första problemet, sätt FIRST = True
+#  3. I slutet av denna fil anger du vilka filer du vill jämföra.
+#  4. Ange antal signifikanta siffror som andra parameter efter filnamnet.
 
 DIR1 = 'data\\'
 DIR2 = 'facit\\'
+ROWS = 1000000
+FIRST = False
 
 def ass (a,b,msg=''):
-    if a!=b:
+    if a != b:
         print(' Difference in',msg)
         print('  ',a)
         print('  ',b)
-        #assert a==b
+        if FIRST: assert a == b
 
-def compare(filename):
+def compare(filename,digits = 6):
+    SCI = "{" + f":.{digits}e" + "}"
     start = time.time()
     print()
-    print('Comparing',filename)
+    print('Comparing',filename,'with',digits,'digits')
     csv1 = pd.read_csv(DIR1 + filename + '.csv')
     csv2 = pd.read_csv(DIR2 + filename + '.csv')
     ass(csv1.shape,csv2.shape,'shape')
@@ -33,37 +36,38 @@ def compare(filename):
     for colname in cols:
         if colname in cols1 and colname in cols2:
             print(' Comparing column',colname)
-            dtype1 = csv1[colname].dtype
-            dtype2 = csv1[colname].dtype
-            #if dtype1=='float': continue
             col1 = csv1[colname]
             col2 = csv2[colname]
-            for j in range(min(1000000,rows)):
+            dtype1 = col1.dtype
+            dtype2 = col2.dtype
+            #if dtype1=='float': continue
+            for j in range(min(ROWS,rows)):
                 cell1 = col1[j]
                 cell2 = col2[j]
                 if dtype1 == 'float':
-                    ass(f'{cell1:.8e}',f'{cell2:.8e}', f"[{colname},{j}]")
+                    ass(SCI.format(cell1),SCI.format(cell2), f"[{colname},{j}]")
                 else:
                     ass(cell1,cell2,f"[{colname},{j}]")
         else:
             print(' Missing column',colname)
-    print(" CPU:",f'{(time.time()-start):3f}')
+    print(f' {rows} rows and {len(cols)} cols compared in {(time.time()-start):.3f} seconds')
 
-def simple(filename):
+def simple(filename,digits = 6):
+    SCI = "{:." + str(digits) + "e}"
     print()
-    print('Comparing simple',filename)
+    print('Comparing simple',filename,'with',digits,'digits')
     with open(DIR1 + filename + '.csv') as f: lines1 = f.readlines()
     with open(DIR2 + filename + '.csv') as f: lines2 = f.readlines()
     ass(len(lines1),len(lines2),'number of lines')
-    for i in range(min(1000000,len(lines1))):
+    for i in range(min(ROWS,len(lines1))):
         cell1 = float(lines1[i])
         cell2 = float(lines2[i])
-        ass(f'{cell1:.7e}', f'{cell2:.7e}', f"[{i}]")
+        ass(SCI.format(cell1), SCI.format(cell2), f"[{i}]")
 
-if 1: simple('skattning_ASCs_hp')
-if 1: simple('skattning_ASCs_rp')
-if 1: compare('Resultat_smabiljett')
-if 1: compare('kalibreringsfaktorer_hp')
-if 1: compare('kalibreringsfaktorer_rp')
-if 1: compare('resandedata_long_hp_nulage') # 60 MB
-if 1: compare('resandedata_long_rp_nulage') # 60 MB
+simple('skattning_ASCs_hp',8)
+simple('skattning_ASCs_rp',8)
+compare('Resultat_smabiljett',8)
+compare('kalibreringsfaktorer_hp',8)
+compare('kalibreringsfaktorer_rp',8)
+compare('resandedata_long_hp_nulage',3) # 60 MB
+compare('resandedata_long_rp_nulage',3) # 60 MB
